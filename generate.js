af600c0b88d43a30326f14d8f5dfdf0ab01803c2
Sergey <bashkin2013@mail.ru>
2020-03-30 21:52:09 +0400

    refactor: textarea generation has moved over function

--- generate.js
+++ generate.js
@@ -1,16 +1,16 @@
 const keyboardRowsRu = [['ё', [1, '!'], [2, '"'], [3, '№'], [4, ';'], [5, '%'], [6, ':'], [7, '?'], [8, '*'], [9, '('], [0, ')'], ['-', '_'], ['=', '+'], 'Backspace'], ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', ['\\', '/'], 'Del'], ['Caps Lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter'], ['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', '▲ ', 'Shift'], ['Ctrl', 'Win', 'Alt', ' ', 'Alt', '◄ ', '▼ ', '► ', 'Ctrl ']];
 const keyboardRowsEn = [['`', [1, '!'], [2, '@'], [3, '#'], [4, '$'], [5, '%'], [6, '^'], [7, '&'], [8, '*'], [9, '('], [0, ')'], ['-', '_'], ['=', '+'], 'Backspace'], ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', ['[', '{'], [']', '}'], ['\\', '|'], 'Del'], ['Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', [';', ':'], ['\'', '"'], 'Enter'], ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', [',', '<'], ['.', '>'], ['/', '?'], '▲ ', 'Shift'], ['Ctrl', 'Win', 'Alt', ' ', 'Alt', '◄ ', '▼ ', '► ', 'Ctrl ']];
+
+const wrapper = document.createElement('div');
+wrapper.classList.add('wrapper');
+const text = document.createElement('div');
+text.classList.add('text');
+const textarea = document.createElement('textarea');
+textarea.classList.add('text-input');
+text.appendChild(textarea);
+wrapper.appendChild(text);
+
 function generateKeyboard(keyboardRows) {
-  const wrapper = document.createElement('div');
-  wrapper.classList.add('wrapper');
-
-  const text = document.createElement('div');
-  text.classList.add('text');
-  const textarea = document.createElement('textarea');
-  textarea.classList.add('text-input');
-  text.appendChild(textarea);
-  wrapper.appendChild(text);
-
   const keyboard = document.createElement('div');
   keyboard.classList.add('keyboard');
   keyboardRows.forEach((e) => {
